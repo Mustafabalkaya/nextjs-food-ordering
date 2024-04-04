2432c1824fb393b296ab46be608b20633e35a0c9
Mustafabalkaya <balkayamustafa99@gmail.com>
2024-04-04 18:55:38 +0300

    first commit

---
+++ components/Campaigns.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Title from "./ui/Title";
+import { MdShoppingCart } from "react-icons/md";
+
+const CampaignItem = () => {
+  return null;
+};
+
+const Campaigns = () => {
+  return null;
+};
+
+export default Campaigns;
